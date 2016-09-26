a7a6e2fd0a31c9f9665d85a9974c15ba724f1b83
yuncliu <lyc1990@qq.com>
2016-09-26 03:22:00 -0400

    use jdbctemplate replace use jdbc directly

--- hello1/src/main/java/hello1/JdbcmyUserDAO.java
+++ hello1/src/main/java/hello1/JdbcmyUserDAO.java
@@ -5,18 +5,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.RowMapper;
 
 import hello1.myUser;
 import hello1.myUserDAO;
 
-class JdbcmyUserDAO implements myUserDAO {
+class JdbcmyUserDAO extends JdbcDaoSupport implements myUserDAO {
+    /*
     private DataSource dataSource;
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+    */
     public void insert(myUser user) {
     }
     public myUser findById(int id) {
+        /*  use jdbc directly
         String sql = "select id, name, age from myuser where id = ?";
         Connection conn = null;
         try {
@@ -37,6 +42,19 @@ class JdbcmyUserDAO implements myUserDAO {
         }catch(SQLException e) {
         }finally {
         }
-        return null;
+        */
+
+        // use JdbcTemplate
+        myUser a = this.getJdbcTemplate().queryForObject(
+                "select id, name, age from myuser where id = ?",
+                new Object[]{id},
+                new RowMapper<myUser>() {
+                        public myUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+                            myUser u = new myUser(rs.getInt("id"), rs.getString("name"), rs.getInt("age"));
+                            return u;
+                        }
+                    }
+                );
+        return a;
     }
 }
